c72f6b02d8fa03e88f7edbc881d697ba2775c5cc
Deeksha Shekar <deeksha.somashekar@gmail.com>
2025-11-22 14:59:21 +0530

    feat: added the hover effect

--- app/Home/Stats.tsx
+++ app/Home/Stats.tsx
@@ -1,7 +1,26 @@
+import { CardSpotlight } from "@/components/ui/card-spotlight";
+
 const Stats = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <CardSpotlight className="h-96 w-96">
+        <p className="text-xl font-bold relative z-20 mt-2 text-white">
+          Authentication steps
+        </p>
+        <div className="text-neutral-200 mt-4 relative z-20">
+          Follow these steps to secure your account:
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem
+            placeat quasi eveniet dicta quisquam reiciendis ipsa vero numquam
+            assumenda, quis, cupiditate nemo mollitia? Totam excepturi aliquid,
+            ut amet sed repellendus.
+          </p>
+        </div>
+        <p className="text-neutral-300 mt-4 relative z-20 text-sm">
+          Ensuring your account is properly secured helps protect your personal
+          information and data.
+        </p>
+      </CardSpotlight>
     </div>
   );
 };
